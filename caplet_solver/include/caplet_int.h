440952adb2a18f857058e464871b4b332462b3d4
Matt Liberty <mliberty@precisioninno.com>
2025-07-20 13:59:58 -0700

    mv arch/flat/side out of header
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- caplet_solver/include/caplet_int.h
+++ caplet_solver/include/caplet_int.h
@@ -125,65 +125,6 @@ float intZXXY(float* coord1[nDim][nBit],
 //*
 double calColD(float* p1[nDim][nBit], float* p2[nDim][nBit]);
 
-//**********************************
-//*
-//* Shape functions
-//*
-//*
-inline float flat(float x, float w)
-{
-  return 1;
-}
-
-inline float arch(float x, float w)
-{
-//* Constant arch shapes are good enough for most of the time
-#ifdef CAPLET_FLAT_ARCH
-  return 0.5;
-#endif
-
-#ifdef DEBUG_SHAPE_BOUNDARY_CHECK
-  if (x > 1.41e-6 || x < 0 || w < 0) {
-    std::cerr << "ERROR: input argument is out of the arch boundary"
-              << std::endl;
-  }
-#endif
-
-  //* Extracted for arch length = 0.4um
-  if (w > 10e-6) {
-    w = 10e-6;
-  }
-  return (-1.1065e-06 + (241.98 + 5.1765e+06 * w) * w
-          + (350.58 + -2.3076e+08 * x + -5.5044e+07 * w) * x)
-         / (-1.0785e-05 + (404.46 + (2.6513e+06 + 2.7367e+11 * w) * w) * w
-            + (456.37 + (1.8301e+08 + -3.3833e+14 * x) * x
-               + (-1.0181e+07 + 5.4154e+12 * w + 1.0317e+15 * x) * w)
-                  * x);
-}
-
-inline float side(float x, float w)
-{
-#ifdef CAPLET_FLAT_SIDE
-  return 0.5;
-#endif
-
-#ifdef DEBUG_SHAPE_BOUNDARY_CHECK
-  if (x > 1.251e-6 || x < 0 || w < 0) {
-    std::cerr << "ERROR: input argument is out of the arch boundary"
-              << std::endl;
-  }
-#endif
-
-  //* Extracted for arch length = 0.4um
-  if (w > 10e-6) {
-    w = 10e-6;
-  }
-  return (0.00012065 + (237.09 + -8.6825e+06 * w) * w
-          + (1580.3 + -1.7957e+09 * x + -2.3054e+07 * w) * x)
-         / (0.00029255 + (398.95 + -1.6467e+07 * w) * w
-            + (2789.4 + 1.447e+09 * x + 1.0577e+08 * w) * x);
-}
-
 }  // namespace caplet
 
 #endif /* CAPLET_INT_H_ */
--- caplet_solver/src/caplet.cpp
+++ caplet_solver/src/caplet.cpp
@@ -49,6 +49,65 @@ using namespace std;
 float Caplet::epsilon0 = 8.8541878176e-12f;
 float Caplet::pi = 3.1415926535f;
 
+//**********************************
+//*
+//* Shape functions
+//*
+//*
+static float flat(const float x, const float w)
+{
+  return 1;
+}
+
+static float arch(const float x, float w)
+{
+//* Constant arch shapes are good enough for most of the time
+#ifdef CAPLET_FLAT_ARCH
+  return 0.5;
+#endif
+
+#ifdef DEBUG_SHAPE_BOUNDARY_CHECK
+  if (x > 1.41e-6 || x < 0 || w < 0) {
+    std::cerr << "ERROR: input argument is out of the arch boundary"
+              << std::endl;
+  }
+#endif
+
+  //* Extracted for arch length = 0.4um
+  if (w > 10e-6) {
+    w = 10e-6;
+  }
+  return (-1.1065e-06 + (241.98 + 5.1765e+06 * w) * w
+          + (350.58 + -2.3076e+08 * x + -5.5044e+07 * w) * x)
+         / (-1.0785e-05 + (404.46 + (2.6513e+06 + 2.7367e+11 * w) * w) * w
+            + (456.37 + (1.8301e+08 + -3.3833e+14 * x) * x
+               + (-1.0181e+07 + 5.4154e+12 * w + 1.0317e+15 * x) * w)
+                  * x);
+}
+
+static float side(const float x, float w)
+{
+#ifdef CAPLET_FLAT_SIDE
+  return 0.5;
+#endif
+
+#ifdef DEBUG_SHAPE_BOUNDARY_CHECK
+  if (x > 1.251e-6 || x < 0 || w < 0) {
+    std::cerr << "ERROR: input argument is out of the arch boundary"
+              << std::endl;
+  }
+#endif
+
+  //* Extracted for arch length = 0.4um
+  if (w > 10e-6) {
+    w = 10e-6;
+  }
+  return (0.00012065 + (237.09 + -8.6825e+06 * w) * w
+          + (1580.3 + -1.7957e+09 * x + -2.3054e+07 * w) * x)
+         / (0.00029255 + (398.95 + -1.6467e+07 * w) * w
+            + (2789.4 + 1.447e+09 * x + 1.0577e+08 * w) * x);
+}
+
 //__________________________________________________________
 //*
 //* Ctor and Dtor
